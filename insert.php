9a83dcf232a9819f7ee58e81659061655e64c09d
TwilightGalaxy <andyzhenghan@gmail.com>
2020-07-08 19:24:24 -0700

    add php
    add insert.php file and change form component names in index.html

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+
+$mongo = new MongoClient();
+$db = $mongo->donalddump;
+$collection = $db->info;
+if($_POST) {
+    $insert = array(
+        'name' => $_POST['name'];
+        'email' => $_POST['email'];
+        'message' => $_POST['message'];
+    );
+
+    if ($collection->insert($insert)) {
+        echo "data is inserted";
+    } else {
+        echo "we have some issue";
+    }
+} else {
+    echo "no data to store";
+}
+
+?>
